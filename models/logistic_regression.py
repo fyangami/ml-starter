caa4d1d0f9fa31ebfbfbee7b8dc249a04298c066
fyang <fyang@fe1.xyz>
2022-03-11 16:07:49 +0800

    logistic regression with multi classes

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -4,21 +4,19 @@ import numpy as np
 
 class LogisticClassifier:
 
-    def __init__(self, eta=1e-3, epochs=50, batch_size=20, seed=1024) -> None:
+    def __init__(self, eta=1e-3, epochs=50, batch_size=20, seed=1024, verbose=True) -> None:
         self.eta = eta
         self.epochs = epochs
         self.batch_size = batch_size
         self.__seed = seed
+        self.verbose = verbose
     
     def logic(self, x: ndarray) -> ndarray:
-        # 20 x 20  20 x 1
-        print(x.shape)
-        print(self.w.shape)
         liner = x.dot(self.w) + self.b
         return 1 / (1 + np.exp(-liner))
     
     def loss(self, y_hat, y) -> ndarray:
-        return (y_hat - y) ** 2 / 2
+        return -((y * np.log(y_hat)) + ((1 - y) * np.log(1 - y_hat))).mean()
     
     def gradients(self, x, y_hat, y):
         return (y_hat - y).T.dot(x), (y_hat - y)
@@ -26,7 +24,6 @@ class LogisticClassifier:
     def fit(self, x, y, tx, ty) -> None:
         np.random.seed(self.next_seed())
         self.w = np.random.randn(x.shape[1], 1)
-        self.next_seed()
         self.b = np.random.randn(1)
         self.losses = np.zeros(self.epochs)
         from utils.data import data_genterator
@@ -34,13 +31,58 @@ class LogisticClassifier:
             for x, y in data_genterator(x, y, self.next_seed(), self.batch_size):
                 y_hat = self.logic(x)
                 dw, db = self.gradients(x, y_hat, y)
-                self.w = self.w - (self.eta * dw / len(x)).mean(axis=0)
-                self.b = self.b - (self.eta * db / len(x)).mean(axis=0)
+                self.w = self.w - (self.eta * dw / len(x)).mean(axis=0).reshape(-1, 1)
+                self.b = self.b - (self.eta * db / len(x)).mean(axis=0).reshape(-1, 1)
             l = self.loss(self.logic(tx), ty)
             self.losses[epoch] = np.mean(l)
-            print(f'epoch: {epoch + 1} --- loss: {self.losses[epoch]}')
+            if self.verbose:
+                print(f'epoch: {epoch + 1} --- loss: {self.losses[epoch]}')
     
     def next_seed(self):
         seed = self.__seed
         self.__seed += 1
         return seed
+
+
+class MultiLogisticClassifier:
+    
+    def __init__(self, eta=1e-3, epochs=50, batch_size=20, seed=1024, verbose=False) -> None:
+        self.eta = eta
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.__seed = seed
+        self.verbose=verbose
+
+
+    def fit(self, x, y, tx, ty):
+        classes = np.unique(y)
+        print(f'got classes: {classes}, ues one to many classifier')
+        self.models = list()
+        for i, n_class in enumerate(classes):
+            n_model = LogisticClassifier(self.eta, self.epochs, self.batch_size, self.__seed, verbose=self.verbose)
+            n_model.fit(x, (y == n_class).astype('uint8').reshape(-1, 1), tx, (ty == n_class).astype('uint8').reshape(-1, 1))
+            self.models.append(n_model)
+            print(f'iter[{i}, complete == {(i + 1) / len(classes) * 100}%]')
+
+    def predict(self, x):
+        predicted = np.zeros(len(x))
+        for n_class, model in enumerate(self.models):
+            n_predcit = np.round(model.logic(x))
+            for idx, predict in enumerate(n_predcit):
+                if predict == 1.:
+                    predicted[idx] = n_class
+            
+        return predicted
+
+    def score(self, x):
+        predicted = None# np.zeros((x,len(self.model_map)))
+        for model in self.models:
+            n_predict = model.logic(x)
+            if predicted is None:
+                predicted = n_predict
+            else:
+                predicted = np.c_[predicted, n_predict]
+        return predicted
+    
+    def losses(self, idx) -> ndarray:
+        return self.models[idx].losses
--- utils/data.py
+++ utils/data.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def data_genterator(x, y, seed=1024, batch_size=20):
     n_ = len(x)
@@ -8,4 +9,13 @@ def data_genterator(x, y, seed=1024, batch_size=20):
     for batch in range(0, n_, batch_size):
         batcher = indicates[batch: min(batch + batch_size, n_)]
         yield x[batcher, :], y[batcher]
+
+def one_hot(df: pd.DataFrame, cols, drop=True):
+    for col in cols:
+        unis = np.unique(df[col])
+        for uni in unis:
+            df[col + '_' + str(uni)] = (df[col] == uni).astype('uint8')
+        if drop:
+            df.drop(col, axis=1, inplace=True)
+        
     
